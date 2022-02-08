00008c1c7fe86ca5a172523de6bcae7ae77dd743
Stanley Pan <imoment.developer@gmail.com>
2022-02-08 15:55:34 -0500

    Initial Commit

---
+++ ToolbarUI/ToolbarUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToolbarUIApp.swift
+//  ToolbarUI
+//
+//  Created by Stanley Pan on 2022/02/08.
+//
+
+import SwiftUI
+
+@main
+struct ToolbarUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
